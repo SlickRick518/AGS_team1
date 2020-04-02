4ecb1382819718f92cfaf7c12ed94acc9afa674e
djdramamine <dushar88@students.rowan.edu>
2020-04-01 21:06:49 -0400

    added post languages request

--- node-server/agsteam1api/routes/api.js
+++ node-server/agsteam1api/routes/api.js
@@ -16,4 +16,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'importUtil' });
 });
 
+
+/**
+ * --POST--
+ * purpose
+ *  Adds a language to the Languages Table to the Database
+ * 
+ * Endpoint 
+ *  /languages
+ *  
+ * params
+ *  lang_id: int
+ *  lang_name: string(20)
+ *  emp_id: int
+*/
+router.post('/langaguges', function(req, res, next) {
+  let sql = "CALL addLanguage("+req.body.lang_id+","+req.body.lang_name+","+req.body.emp_id+")";
+  con.query(sql, function(req, res, next) {
+    if (err) console.log(err);
+    res.send(result);
+  })
+});
+
 module.exports = router;
